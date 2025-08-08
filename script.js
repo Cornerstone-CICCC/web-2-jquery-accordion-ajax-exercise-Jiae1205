2b234fe62cec343a94397358c77399e6816c7758
Jiae Park <wldo7237@gmail.com>
2025-08-08 15:04:25 -0700

    ex

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(function() {
-  // your code here
-})
+  // Exercise 1
+  $('.accordion-header').on('click', function() {
+    $('.accordion-header').not(this).next('.accordion-content').slideUp();
+    $(this).next('.accordion-content').slideToggle('fast', function() {
+      console.log('Content Toggled')
+    });
+  });
+
+  // Exercise 2
+  $('.todos button').on('click', function() {
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      method: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        const $ul = $('.todos ul').empty();
+        response.todos.forEach(todo => {
+          $ul.append($('<li>').text(todo.todo));
+        });
+      },
+      error: function() {
+        alert('Failed to load to-do items.');
+      }
+    });
+  });
+});
